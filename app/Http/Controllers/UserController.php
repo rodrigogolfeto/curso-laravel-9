a9ed426fa4b762be93affa79ccf6c0106adc0ce9
Rodrigo Golfeto <rgolfeto@gmail.com>
2023-07-02 13:01:01 -0300

    Listando registros do banco de dados

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index(){
-        return view('users.index');
+
+        $users = User::get();
+
+        // return view('users.index',['users' => $users]);
+        return view('users.index',compact('users'));
     }
 
     public function show($id){
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,1 +1,9 @@
-Listagem dos usuários
+<h1>Listagem dos usuários</h1>
+
+<ul>
+  @foreach ($users as $user)
+      <li>
+        {{ $user->name }} - {{ $user->email }}
+      </li>
+  @endforeach
+</ul>
